884d2dc289f4dc2eabc79970fa30abb208d74449
izattullah <116265920+izattullah@users.noreply.github.com>
2024-04-28 21:34:02 +0300

    Edited trainging user can see customer name with trainging

--- src/components/Training.jsx
+++ src/components/Training.jsx
@@ -4,51 +4,57 @@ import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-material.css";
 import dayjs from "dayjs";
 
-export default function Training(){
+export default function Training() {
 
     const [trainings, setTrainings] = useState([{
-        activity:'', date:'', duration:'', customer:''
+        activity: '', date: '', duration: '', customer: ''
 
-}]);
+    }]);
 
-const [columnDefs, setColumnDefs] = useState([
-    { field: 'activity', headerName:'Activity', sortable: true, filter: true},
-    { field: 'date', headerName:'Date', sortable: true, filter: true,
-    valueFormatter: params => 
-    dayjs(params.value).format('DD/MM/YYYY HH:mm')},
-    {field: 'duration' , headerName:'Duration', sortable: true, filter: true},
-    {field: 'customer', headerName:'Customer', sortable: true, filter: true},    
-]);
+    const [columnDefs, setColumnDefs] = useState([
+        { field: 'activity', headerName: 'Activity', sortable: true, filter: true },
+        {
+            field: 'date', headerName: 'Date', sortable: true, filter: true,
+            valueFormatter: params =>
+                dayjs(params.value).format('DD/MM/YYYY HH:mm')
+        },
+        { field: 'duration', headerName: 'Duration', sortable: true, filter: true },
+        {
+            field: 'customer', headerName: 'Customer', sortable: true, filter: true,
+            valueGetter: params => `${params.data.customer.firstname} ${params.data.customer.lastname}`
+        },
+    ]);
 
-useEffect(() => {
-    getTrainings()}, []); //fetch only after the first rendering
+    useEffect(() => {
+        getTrainings()
+    }, []); //fetch only after the first rendering
 
 
     // functions
     //gettrainings
     const getTrainings = () => {
-        fetch("https://customerrestservice-personaltraining.rahtiapp.fi/api/trainings", { method: 'GET' })
+        fetch("https://customerrestservice-personaltraining.rahtiapp.fi/gettrainings", { method: 'GET' })
             .then(response => {
-                console.log(response);
+
                 return response.json();
             })
             .then(responseData => {
-                console.log(responseData._embedded.trainings)
-                setTrainings(responseData._embedded.trainings);
+
+                setTrainings(responseData);
             })
             .catch(error => console.error(error))
     }
 
     return (
         <>
-        <div className="ag-theme-material" style={{width: 1450, height: 500}}>
-      <AgGridReact 
-        rowData={trainings}
-        columnDefs={columnDefs}
-        pagination={true}
-      />
-    </div> 
-            
+            <div className="ag-theme-material" style={{ width: 1450, height: 500 }}>
+                <AgGridReact
+                    rowData={trainings}
+                    columnDefs={columnDefs}
+                    pagination={true}
+                />
+            </div>
+
         </>
     );
 
